6b4fde899f38a33698a4bf6c6b96bebad051abca
PavelNPavlov <pavelnapavlov@gmail.com>
2015-07-23 10:12:57 +0300

    update
    update

--- PhaserTest/scripts/script1.js
+++ PhaserTest/scripts/script1.js
@@ -18,32 +18,34 @@ function preload() {
 
 var platforms,
     background,
-    player;
+    player,
+    velocityScale=175;;
 
 function create() {
     createWorld();
     createPlatforms();
     createPlayer();
 }
-var controller;
+var controller,
+    canJump=true;
 function update() {
     createController();
     playerUpdate();
     cameraUpdate();
-    console.log(player.x);
+    console.log(player.body.gravity.y);
 }
 
 function createWorld(){
 
     //worldSize
-    game.world.setBounds(0, 0, 2400, 600);
+    game.world.setBounds(0, 0, 2400, 900);
 
     //Physics
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     //BackGround
     background = game.add.sprite(0, 0, 'sky');
-    background.scale.setTo(3,1);
+    background.scale.setTo(3,1.5);
 
     //PlaceHolder
 
@@ -62,10 +64,30 @@ function createPlatforms(){
     var ground = platforms.create(0, game.world.height - 64, 'ground');
     ground.scale.setTo(6, 2);
     ground.body.immovable = true;
-
+    SegmentOne();
     //addPlatforms: to be implemented
 
 
+}
+function SegmentOne(){
+    var ledge1 = platforms.create(400, 900 - 200, 'ground');
+    ledge1.body.immovable = true;
+
+    var ledge2 = platforms.create(600, 900 - 350, 'ground');
+    ledge2.body.immovable = true;
+    ledge2.scale.setTo(0.5,1);
+
+    var ledge3 = platforms.create(0, 900 - 400, 'ground');
+    ledge3.body.immovable = true;
+
+    var ledge4 = platforms.create(500, 900 - 550, 'ground');
+    ledge4.body.immovable = true;
+    ledge4.scale.setTo(0.75,1);
+
+    var wall1 = platforms.create(650, 900 - 550, 'ground');
+    wall1.body.immovable = true;
+    wall1.scale.setTo(0.2,0.5);
+    wall1.angle=270;
 }
 function createPlayer(){
     //sprite: placeHolder
@@ -73,7 +95,7 @@ function createPlayer(){
     //physics
     game.physics.arcade.enable(player);
     //gravity
-    player.body.gravity.y = 400;
+    player.body.gravity.y = 500;
     player.body.collideWorldBounds = true;
 
     //animation: placeHolder
@@ -87,26 +109,53 @@ function createController(){
 function playerUpdate(){
     //collide with ground and platforms
     game.physics.arcade.collide(player, platforms);
-    player.body.velocity.x=0;
+    //retain speed in air or make it 0 when touching ground
+    if(player.body.touching.down){
+        player.body.velocity.x=0;
+        player.body.gravity.x=0;
+        //player.body.gravity.y=500
+
+    }
+
     //movement left/right
     if (controller.left.isDown) {
         //  Move to the left
-        player.body.velocity.x = -150;
+        player.body.velocity.x = -velocityScale;
 
-        // player.animations.play('left');
         player.animations.play('left');
     }  else if (controller.right.isDown) {
         //  Move to the right
-        player.body.velocity.x = 150;
+        player.body.velocity.x = velocityScale;
 
         player.animations.play('right');
-        //player.animations.play('john-right');
     }  else  {
         //  Stand still
         player.animations.stop();
 
         player.frame = 4;
     }
+    //movement jump
+    //prevent continuous jumping
+    if(player.body.touching.down && !controller.up.isDown){
+        canJump=true;
+    }
+    //  Allow the player to jump if they are touching the ground.
+    if (controller.up.isDown && player.body.touching.down && canJump) {
+        player.body.velocity.y = -350;
+        canJump=false;
+
+    }
+    if (controller.up.isDown) {
+       player.body.gravity.y=300;
+    } else {
+        player.body.gravity.y=500;
+    }
+    //resistance in x
+    var speed= Math.abs(player.body.velocity.x);
+    if(speed>0){
+        player.body.gravity.x= (speed/player.body.velocity.x)*25*-1;
+    }
+
 }
 function cameraUpdate(){
 
@@ -118,4 +167,5 @@ function cameraUpdate(){
         game.camera.x=SCREENSIZE*3 - SCREENSIZE/2;
     }//right most camera
     game.camera.x=player.x - SCREENSIZE/2;
+    game.camera.y=player.y - 300;
 }
